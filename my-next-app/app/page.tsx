c6d8aa49c3cfc7a09365d8ac68d86a68c8d1e43a
Andrei536 <125765522+Andrei536@users.noreply.github.com>
2025-06-02 17:58:05 +0200

    Next

---
+++ my-next-app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '400px', fontWeight: 'bold', fontSize: '24px' }}>
+    <main>
+      <h1>Myronov Andrii, 124709</h1>
+    </main>
+    </div>
+  );
+}
